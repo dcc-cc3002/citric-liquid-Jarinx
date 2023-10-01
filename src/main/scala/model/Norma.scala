fa016c15a7ce3fde7800d7621f3dca180a0fe275
Jarinx <ju.ayelli@gmail.com>
2023-09-30 22:16:10 -0300

    enunciados ordenados, setters y getters en Norma junto a tests

--- src/main/scala/model/Norma.scala
+++ src/main/scala/model/Norma.scala
@@ -3,15 +3,54 @@ package model
 
 /** A class that represents a certain Norma to be achieved
  *
- * @param number: number of the level
- * @param neededStars: stars needed to level up
- * @param neededWins: wins needed to level up
+ * @param _number: number of the level
+ * @param _neededStars: stars needed to level up
+ * @param _neededWins: wins needed to level up
  *
  * @author Julieta Ayelli
  */
-class Norma(var number: Int,
-            var neededStars: Int,
-            var neededWins: Int){
+class Norma(private var _number: Int,
+            private var _neededStars: Int,
+            private var _neededWins: Int){
+
+  /** gets the current number of the player's Norma */
+  def number: Int = _number
+
+  /** gets the current number of needed stars of the player to level up a Norma */
+  def neededStars: Int = _neededStars
+
+  /** gets the current number of needed wins of the player to level up a Norma */
+  def neededWins: Int = _neededWins
+
+  /** Sets (updates) the number of the player's Norma
+   * You update the number when the player meets the conditions of Norma Check
+   * @param newNumber the new number of the Norma
+   * @return the new number of the Norma
+   */
+  def setNumber(newNumber: Int): Int = {
+    _number = newNumber
+    return _number
+  }
+
+  /** Sets (updates) the number of needed stars to level up
+   * You update the number when the player meets the conditions of Norma Check
+   * @param newNeedStars the new number of needed stars
+   * @return the updated number of needed stars
+   */
+  def setNeedStars(newNeedStars: Int): Int = {
+    _neededStars = newNeedStars
+    return _neededStars
+  }
+
+  /** Sets (updates) the number of needed wins to level up
+   * You update the number when the player meets the conditions of Norma Check
+   * @param newNeedWins the new number of wins
+   * @return the updated number of needed wins
+   */
+  def setNeedWins(newNeedWins: Int): Int = {
+    _neededWins = newNeedWins
+    return _neededWins
+  }
 
   /** Checks if the player meets the conditions to level up
    *
@@ -20,10 +59,10 @@ class Norma(var number: Int,
    * @return true or false
    */
   def normaCheck(playerStars: Int, playerWins: Int): Boolean = {
-    if (playerStars == neededStars){
+    if (playerStars == _neededStars){
       return true
     }
-    else if (playerWins == neededWins){
+    else if (playerWins == _neededWins){
       return true
     }
     else{
@@ -31,11 +70,9 @@ class Norma(var number: Int,
     }
   }
 
-  /** If the player meets the conditions, they level up
-   *
-   */
+  /** If the player meets the conditions (normaCheck is true), they level up */
   def normaClear(): Unit = {
-    number += 1
+    _number += 1
   }
 
 }
--- src/test/scala/model/NormaTest.scala
+++ src/test/scala/model/NormaTest.scala
@@ -2,14 +2,14 @@ package cl.uchile.dcc.citric
 package model
 
 class NormaTest extends munit.FunSuite {
-  private val number = 2
-  private val neededStars = 10
-  private val neededWins = 1
+  private val _number = 2
+  private val _neededStars = 10
+  private val _neededWins = 1
 
   private var norma: Norma = _
 
   override def beforeEach(context: BeforeEach): Unit = {
-    norma = new Norma(number, neededStars, neededWins)
+    norma = new Norma(_number, _neededStars, _neededWins)
   }
 
   test("To level up to Norma 2, a player needs a certain amount of stars (10) or wins (1)"){
@@ -18,7 +18,19 @@ class NormaTest extends munit.FunSuite {
 
   test("If the conditions are met, a player levels up"){
     norma.normaClear()
-    assertEquals(norma.number, number)
+    assertEquals(norma.number, _number+1)
+  }
+
+  test("You can get the attributes"){
+    assertEquals(norma.number, _number)
+    assertEquals(norma.neededStars, _neededStars)
+    assertEquals(norma.neededWins, _neededWins)
+  }
+
+  test("You can update the attributes, when needed"){
+    assertEquals(norma.setNumber(3), _number+1)
+    assertEquals(norma.setNeedStars(30), _neededStars+20)
+    assertEquals(norma.setNeedWins(3), _neededWins+2)
   }
 
 }
