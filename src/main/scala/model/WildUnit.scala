1ecc330c966f245458fa730f5f77340c582d6d7a
Jarinx <ju.ayelli@gmail.com>
2023-09-30 23:38:12 -0300

    getters y setters WU y sus tests

--- src/main/scala/model/WildUnit.scala
+++ src/main/scala/model/WildUnit.scala
@@ -10,7 +10,30 @@ import cl.uchile.dcc.citric.model.Entity
  *
  * @author Julieta Ayelli
  */
-class WildUnit (val enemy: String,
-                var stars: Int) extends Entity {
+class WildUnit (var _enemy: String,
+                var _stars: Int) extends Entity {
+
+  /** gets the current type of Wild Unit */
+
+  def enemy: String = _enemy
+
+  /** gets the current number of stars accumulated */
+
+  def stars: Int = _stars
+
+  /** sets (updates) the enemy*/
+  def setEnemy(newEnemy: String): String = {
+    _enemy = newEnemy
+    return _enemy
+  }
+
+  /** sets (updates) the number of stars
+  * it's protected because only subclasses can access it to, for example,
+   * update the number of stars they have*/
+  protected def setStars(newStars: Int): Int = {
+    _stars = newStars
+    return _stars
+  }
+
 
 }
--- src/test/scala/model/WildUnitTest.scala
+++ src/test/scala/model/WildUnitTest.scala
@@ -2,29 +2,29 @@ package cl.uchile.dcc.citric
 package model
 
 class WildUnitTest extends munit.FunSuite {
-  private val enemy = "Chicken"
+  private val _enemy = "Chicken"
   private val maxHp = 10
   private val currentHp = 10
   private val attack = 1
   private val defense = 1
   private val evasion = 1
-  private val stars = 7
+  private val _stars = 7
 
   private var badGuy: WildUnit = _
   private var character: PlayerCharacter = _
   override def beforeEach(context: BeforeEach): Unit = {
-    badGuy = new WildUnit(enemy, stars)
+    badGuy = new WildUnit(_enemy, _stars)
   }
 
   //tests
-  test("A wild unit should have correctly set their atttributes"){
-    assertEquals(badGuy.enemy, enemy)
+  test("A wild unit should have correctly set their attributes"){
+    assertEquals(badGuy.enemy, _enemy)
     assertEquals(badGuy.maxHp, maxHp)
     assertEquals(badGuy.currentHp, currentHp)
     assertEquals(badGuy.attack, attack)
     assertEquals(badGuy.defense, defense)
     assertEquals(badGuy.evasion, evasion)
-    assertEquals(badGuy.stars, stars)
+    assertEquals(badGuy.stars, _stars)
   }
 
   test("A wild unit can take a certain amount of damage from a player"){
@@ -33,6 +33,12 @@ class WildUnitTest extends munit.FunSuite {
 
   test("A wild unit can do a certain amount of damage to a player"){
     assertEquals(badGuy.doDmg(character, 5), 5)
+  }
+   test("You can set a Wild Unit, for example, to a certain Encounter Panel"){
+     assertEquals(badGuy.setEnemy("Robo ball"), "Robo ball")
+   }
+  test("You can´t change the number of stars a Wild Unit has (TEST FAILING IS OK"){
+    assertEquals((badGuy.setStars(5)), 5)
   }
 }
 
